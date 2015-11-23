52cc95075a190565fec93ea14a315f5c65f449ff
MyroTV <michael.mcdougall92@gmail.com>
2015-11-23 14:59:29 +1100

    0.0.3.7
    resync

--- src/com/me/gdxtestgame/Main.java
+++ src/com/me/gdxtestgame/Main.java
@@ -7,9 +7,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL", "true");
 		cfg.title = "gdx-test-game";
-		cfg.useGL20 = false;
-		cfg.width = 1680;
+		cfg.useGL20 = true;
+		cfg.width = 1020;
 		cfg.height = 1020;
 		
 		new LwjglApplication(new MyGame(), cfg);
